c3dfb5648fdb946fe44a7ae436943d276a8a1eea
Roman <spriggan1992@gmail.com>
2025-03-03 21:35:38 +0700

    fix phone mask

--- lib/src/masks/masked_phone_input_formatter.dart
+++ lib/src/masks/masked_phone_input_formatter.dart
@@ -54,21 +54,20 @@ class MaskedPhoneInputFormatter extends TextInputFormatter {
     final placeholder = List.filled(splitMask.length, '', growable: false);
     var lastRealCharIndex = fixedPrefix.length;
 
-    // Проходим по маске, начиная после префикса
     for (var i = fixedPrefix.length; i < splitMask.length; i++) {
       if (index >= clearedValue.length) break;
       final maskChar = splitMask[i];
+
       if (maskChar == _anyCharMask || maskChar == _onlyDigitMask) {
-        // Это позиция для ввода символа
         final curChar = clearedValue[index];
-        if (maskChar == _onlyDigitMask && !RegExp(r'[0-9]').hasMatch(curChar)) {
-          continue; // Пропускаем недопустимые символы
-        }
+
+        if (maskChar == _onlyDigitMask &&
+            !RegExp(r'[0-9]').hasMatch(curChar)) {}
+
         placeholder[i] = curChar;
         lastRealCharIndex = i + 1;
         index++;
       } else {
-        // Это разделитель
         placeholder[i] = maskChar;
       }
     }
@@ -87,7 +86,6 @@ class MaskedPhoneInputFormatter extends TextInputFormatter {
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    // Если текст полностью удалён или префикс отсутствует, восстанавливаем префикс
     if (newValue.text.length < fixedPrefix.length ||
         !newValue.text.startsWith(fixedPrefix)) {
       return TextEditingValue(
@@ -96,42 +94,31 @@ class MaskedPhoneInputFormatter extends TextInputFormatter {
       );
     }
 
-    // Выделяем текст, который ввёл пользователь (без префикса)
     String inputText = newValue.text.substring(fixedPrefix.length);
 
-    // Если текст пустой и равен префиксу, возвращаем текущее значение
     if (inputText.isEmpty && newValue.text == fixedPrefix) {
       return newValue;
     }
 
-    // Применяем маску к введённому тексту
     final formattedValue = applyMask(fixedPrefix + inputText);
     _maskedValue = formattedValue._formattedValue;
 
-    // Корректируем позицию курсора
     int selectionIndex = newValue.selection.baseOffset;
 
-    // Если пользователь добавляет символ
     if (newValue.text.length > oldValue.text.length) {
       final newChar = newValue.text[newValue.selection.baseOffset - 1];
 
-      // Если символ совпадает с символом из префикса, но находится за его пределами, разрешаем ввод
       if (fixedPrefix.contains(newChar) &&
-          newValue.selection.baseOffset - 1 >= fixedPrefix.length) {
-        // Ничего не делаем, символ будет добавлен в маску
-      }
+          newValue.selection.baseOffset - 1 >= fixedPrefix.length) {}
     }
 
-    // Корректируем позицию курсора с учётом длины отформатированного текста
     final newTextLength = newValue.text.length;
     final formattedTextLength = _maskedValue.length;
     selectionIndex += formattedTextLength - newTextLength;
 
-    // Ограничиваем позицию курсора в пределах текста
     selectionIndex =
         selectionIndex.clamp(fixedPrefix.length, formattedTextLength);
 
-    // Пропускаем разделители при перемещении курсора
     while (selectionIndex < _maskedValue.length &&
         _separators.contains(_maskedValue[selectionIndex])) {
       selectionIndex++;
